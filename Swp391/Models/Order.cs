0093ddbb357fc991fe3f735f9ce45345988a272f
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2024-05-21 13:28:01 +0700

    create models in project

---
+++ Swp391/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Swp391.Models;
+
+public partial class Order
+{
+    public int OrderId { get; set; }
+
+    public DateTime? Date { get; set; }
+
+    public int Status { get; set; }
+
+    public int TableId { get; set; }
+
+    public int StoreId { get; set; }
+
+    public double Total { get; set; }
+
+    public virtual OrderDetail? OrderDetail { get; set; }
+
+    public virtual Store Store { get; set; } = null!;
+
+    public virtual Table Table { get; set; } = null!;
+}
