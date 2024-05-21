71041a770f2b790eb7e9f1ef18f94f2b946f0730
Tronghghe172557 <tronghghe172557@fpt.edu.vn>
2024-05-21 20:59:56 +0700

    change cái gì đó ?

--- Swp391/obj/Debug/net8.0/Swp391.AssemblyInfo.cs
+++ Swp391/obj/Debug/net8.0/Swp391.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Swp391")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+249c834d5ed5dc21fbe6b493c0953f634145c37c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a4e5128ea3e17a12bbc2b224fc98d480d95cc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Swp391")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Swp391")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
