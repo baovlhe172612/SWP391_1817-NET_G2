cc37d25b980e2e78d89ac07126f4126e998e5930
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2024-05-21 14:28:16 +0700

    creata repo, service, setting json

---
+++ Swp391/Controllers/ProductControlles.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swp391.Service;
+
+namespace Swp391.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductControlles : ControllerBase
+    {
+        private ProductsService _service = new ProductsService();
+
+        [HttpGet]
+        public IActionResult getAllProduct()
+        {
+            return Ok(_service.getAllProductUI());
+        }
+    }
+}
