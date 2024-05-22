ee2de073c88f15707370e8214fa0737879c5aecf
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2024-05-22 11:53:11 +0700

    take code

---
+++ Swp391/Dtos/AccountDtos.cs
@@ -0,0 +1,17 @@
+﻿namespace Swp391.Dtos
+{
+    public class AccountDtos
+   {
+        public int AccountId { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public int Status { get; set; }
+        public string? Email { get; set; }
+        public string? FullName { get; set; }
+        public string? Location { get; set; }
+        public string? Phone { get; set; }
+        public int RoleId { get; set; }
+        public string Token { get; set; }
+        public string RoleName { get; set; }
+    }
+}
