ee2890aa3408ca9de36d1f0bc278a7f44243a066
Tronghghe172557 <tronghghe172557@fpt.edu.vn>
2024-05-22 21:14:00 +0700

    feat: tính năng list Store bên BE
    refactor: thêm try catch cho phần component Products

--- Swp391/obj/Debug/net8.0/Swp391.AssemblyInfo.cs
+++ Swp391/obj/Debug/net8.0/Swp391.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Swp391")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd6dc70e9f4437d7793315b12d545ace893078a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8f6bd87963bf8f37a02e0231e47f6009f673885")]
 [assembly: System.Reflection.AssemblyProductAttribute("Swp391")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Swp391")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
