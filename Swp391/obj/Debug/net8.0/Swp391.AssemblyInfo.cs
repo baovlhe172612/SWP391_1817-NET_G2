9e10f485a7a87eb9bb813ea32391c0d11541c68e
Tronghghe172557 <tronghghe172557@fpt.edu.vn>
2024-05-22 12:09:31 +0700

    build: quá trình build

--- Swp391/obj/Debug/net8.0/Swp391.AssemblyInfo.cs
+++ Swp391/obj/Debug/net8.0/Swp391.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Swp391")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b335cf8c85d6cba21843ef2558ac4b3a29b11ed5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcdee47accb812273c73f9c088219e3992941ced")]
 [assembly: System.Reflection.AssemblyProductAttribute("Swp391")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Swp391")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
