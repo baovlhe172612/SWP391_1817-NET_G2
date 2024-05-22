8dbd20b306cd470c34a69c89d502f3b7a5fbae38
kientvhe172514 <kientvhe172514@fpt.edu.vn>
2024-05-22 13:21:51 +0700

    comment account

--- Swp391/Service/AccountService.cs
+++ Swp391/Service/AccountService.cs
@@ -5,7 +5,13 @@ namespace Swp391.Service
 {
     public class AccountService
     {
+
        AccountRepo _accountRepo= new AccountRepo();
+        /// <summary>
+        /// lấy toàn bộ sản phẩm bằng service
+        /// </summary>
+
+        /// <returns>trả về toàn bộ account + roleName</returns>
         public List<AccountDtos> GetAllAccountDtos()
         {
             return _accountRepo.GetAllAccountsAsync();           
