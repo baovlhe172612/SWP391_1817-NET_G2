f9d663b6b5459b1ccd3d2ae2776b03d541cd880d
kientvhe172514 <kientvhe172514@fpt.edu.vn>
2024-05-22 11:43:15 +0700

    DTOS

---
+++ Swp391/Service/AccountService.cs
@@ -0,0 +1,20 @@
+﻿using Swp391.Models;
+using Swp391.Repository;
+using Swp391.Dtos;   
+namespace Swp391.Service
+{
+    public class AccountService
+    {
+        AccountRepo _accountRepo = new AccountRepo();
+
+        /// <summary>
+        /// lấy toàn bộ sản phẩm bằng service
+        /// </summary>
+
+        /// <returns>trả về toàn bộ tài khoản</returns>
+        public async Task<List<AccountDtos>> GetAllAccountAsync()
+        {
+            return await _accountRepo.GetAllAccountsAsync();
+        }
+    }
+}
