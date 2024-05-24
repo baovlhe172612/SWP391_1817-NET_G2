08c1d8a7ceb97f63deaf2a532bfd9469d7d41b42
Tronghghe172557 <tronghghe172557@fpt.edu.vn>
2024-05-24 14:34:24 +0700

    feat: Pagination cho trang listProduct
    Nhưng mà thấy ko ổn, đổi sang làm Đăng nhập

--- FE/src/pages/Admin/ListQrCode/ListQr.js
+++ FE/src/pages/Admin/ListQrCode/ListQr.js
@@ -1,11 +1,21 @@
-import { Row } from "antd";
+import { Col, Row } from "antd";
 import React, { useEffect, useState } from "react";
 import { get } from "../../../helpers/API.helper";
 import Qr from "../../../components/Admin/QR/Qr";
 import { TABLE } from "../../../helpers/APILinks";
+import Pagination from "../../../components/Mixin/Pagination/Pagination";
 
 function ListQr() {
   const [qrs, setQrs] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1)
+
+  // pagination table
+  const paginationTable = {
+    currentPage: currentPage,
+    item: 24,
+    skip: (currentPage - 1) * 24,
+    countPage: 0,
+  }
 
   useEffect(() => {
     const fetchApi = async () => {
@@ -25,6 +35,11 @@ function ListQr() {
     fetchApi();
   }, []);
 
+  // page = 1 => 24
+  // page = 2 => 24 => 48
+  // pagination: count / 24 + 1
+  // skip: (n - 1) * 24 
+
   return (
     <>
       <Row
@@ -36,6 +51,11 @@ function ListQr() {
         }}
       >
         {qrs.length > 0 && qrs.map((qr) => <Qr key={qr.tableId} qr={qr} />)}
+
+        <Col xl={16} md={16} ></Col>
+        <Col xl={8} md={8} >
+          <Pagination />
+        </Col>
       </Row>
     </>
   );
