21db71a0f0a64e446ea84e01d4ff4242bfc6255a
kientvhe172514 <kientvhe172514@fpt.edu.vn>
2024-05-31 19:59:30 +0700

    merge code

--- FE/src/routes/index.routes.js
+++ FE/src/routes/index.routes.js
@@ -35,6 +35,7 @@ import Logout from "../components/Admin/Accounts/Logout/Logout";
 import ProtectedRole from "../components/Admin/ProtectedRole/ProtectedRole";
 import NotFound from "../pages/Admin/404NotFound/NotFound";
 import UpdateStoreManager from "../pages/Admin/ManagerStore/UpdateStoreManager";
+import UpdateCategory from "../pages/Admin/Category/UpdateCategory";
 
 const routes = [
   {
@@ -176,6 +177,10 @@ const routes = [
                   {
                     path: "create",
                     element: <CreateCategory />,
+                  },
+                  {
+                    path: "edit/:id",
+                    element: <UpdateCategory />,
                   },
                 ],
               },
