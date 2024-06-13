533db8b68981e82318a17f9779c9398a07906a33
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2024-06-13 08:28:12 +0700

    update models

---
+++ BE/Models/Conversation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BE.Models;
+
+public partial class Conversation
+{
+    public int ConversationId { get; set; }
+
+    public int? UserChatFirstId { get; set; }
+
+    public int? UserSecondId { get; set; }
+
+    public virtual Message ConversationNavigation { get; set; } = null!;
+
+    public virtual UserChat? UserChatFirst { get; set; }
+
+    public virtual UserChat? UserSecond { get; set; }
+}
