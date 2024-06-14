e336ca50a0c826521b6b122b561465a5d0b05f55
tronghghe172557 <tronghghe172557@fpt.edu.vn>
2024-06-14 10:15:35 +0700

    Build: Xóa hết folder

--- FE/src/helpers/CheckRole.js
+++
@@ -1,3 +0,0 @@
-export const hasRole = (userRole, allowedRoles) => {
-  return allowedRoles.includes(userRole);
-};
--- FE/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import LoginReducer from "./Login";
-
-const allReducers = combineReducers({
-  // thêm nhiều reducer ở đây
-  LoginReducer,
-});
-
-export default allReducers;
