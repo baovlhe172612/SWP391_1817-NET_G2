582386c9b3291d037141c7cfdc0c432ee9ef0fa7
kientvhe172514 <kientvhe172514@fpt.edu.vn>
2024-06-19 10:34:41 +0700

    merge lam

--- FE/src/helpers/APILinks.js
+++ FE/src/helpers/APILinks.js
@@ -8,8 +8,6 @@ export const GET_ACCOUNT_BY_ID = `${LOCALHOST_API}api/Account/`;
 
 // API CATEGORY
 
-
-
 // API MESSENGERBOX
 
 // API POST
@@ -23,39 +21,43 @@ export const DELETE_STORE_ID = `${LOCALHOST_API}/api/stores/delete/`;
 export const STORE_DETAIL = `${LOCALHOST_API}/api/stores/`;
 export const UPDATE_STORE = `${LOCALHOST_API}/api/stores/Update`;
 export const SEARCH_STORE = `${LOCALHOST_API}/api/stores/search`;
-
 // API Category
-
+export const API_CATEGORY = `${LOCALHOST_API}/api/Category`
 export const CREATE_CATEGORY = `${LOCALHOST_API}/api/Category/add_new`;
-
-
 // API ACCOUNTDTOS
 export const GET_ACCOUNT_BY_AUTH = `${LOCALHOST_API}/api/AccountDtos/GET`
 export const GET_ACCOUNT_BY_TOKEN = `${LOCALHOST_API}/api/AccountDtos/GET`
-
 // API TABLE
 export const TABLE = `${LOCALHOST_API}/api/Table`;
 export const LIST_ACCOUNT = `${LOCALHOST_API}/api/Account`;
 export const LIST_TABLE = `${LOCALHOST_API}/api/Table`;
-
 // API PRODUCT CONTROLLERS
+
+
 export const LIST_PRODUCT_DTOS = `${LOCALHOST_API}/api/productDtos`;
 //API ACCOUNT MANAGERS
+
+
 export const LIST_ACCOUNT_MANAGERS = `${LOCALHOST_API}/api/Account/manager`;
 export const CREATE_ACCOUNT_MANAGER = `${LOCALHOST_API}/api/Account`;
 export const UPDATE_ACCOUNT_MANAGER= `${LOCALHOST_API}api/Account/`;
-
 // API PRODUCTDTOS
+
+
 export const CREATE_PRODUCT = `${LOCALHOST_API}/api/ProductSizes/Create`;
 // API PRODUCTSIZES
+export const LIST_FOUR_PRODUCT_SIZE_MIN = `${LOCALHOST_API}/api/ProductSizes/getFourProductMin`
+export const LIST_FOUR_PRODUCT_SIZE_MAX = `${LOCALHOST_API}/api/ProductSizes/getFourProductMax`
+
 export const LIST_PRODUCT_SIZE = `${LOCALHOST_API}/api/ProductSizes`;
 // API TABLE
 
+
 // API FeedBack <=> messegerbox
 export const LIST_FEEDBACK = `${LOCALHOST_API}/api/MessengerBox`
-
 // API EMPLOYEE <=> account
 export const LIST_Employee = `${LOCALHOST_API}/employee`;
 
 // LINK SHOP CLIENT
 export const LIST_PRODUCT = `http://localhost:3000/listProduct/`;
+export const API_ORDER = `${LOCALHOST_API}/api/Order`;
