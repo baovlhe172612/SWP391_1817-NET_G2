71e20ec2264252aa2fc5bbb4730616f97087197f
kientvhe172514 <kientvhe172514@fpt.edu.vn>
2024-06-19 11:45:53 +0700

    merge bao

--- BE/Repository/AccountRepo.cs
+++ BE/Repository/AccountRepo.cs
@@ -290,8 +290,7 @@ namespace Swp391.Repository
             var accountsWithRoles = (from a in _context.Accounts
                                      join r in _context.Roles on a.RoleId equals r.RoleId
                                      join s in _context.Stores on a.StoreId equals s.StoreId
-                                     where r.RoleName == "Employee" && a.IsDelete == 0
-
+                                     where r.RoleName == "Employee" 
                                      select new AccountDtos
                                      {
                                          AccountId = a.AccountId,
