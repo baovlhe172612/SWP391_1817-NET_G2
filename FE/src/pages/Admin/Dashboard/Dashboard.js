94c23fb00abe9aa198934ddffc72c0883567b713
kientvhe172514 <kientvhe172514@fpt.edu.vn>
2024-06-24 16:14:53 +0700

    feat: fix link api

--- FE/src/pages/Admin/Dashboard/Dashboard.js
+++ FE/src/pages/Admin/Dashboard/Dashboard.js
@@ -360,9 +360,9 @@ function Dashboard() {
     try {
       let data;
       if (isOwner) {
-        data = await get("http://localhost:5264/api/Order/daily-revenue");
+        data = await get("http://172.20.10.5:5264/api/Order/daily-revenue");
       } else if (isManager) {
-        data = await get(`http://localhost:5264/api/Order/daily-revenue/${account.storeId}`);
+        data = await get(`http://172.20.10.5:5264/api/Order/daily-revenue/${account.storeId}`);
       }
       console.log("data-revenue", data);
       setRevenue(data);
@@ -379,9 +379,9 @@ function Dashboard() {
     try {
       let data;
       if (isOwner) {
-        data = await get("http://localhost:5264/api/Order/month-revenue");
+        data = await get("http://172.20.10.5:5264/api/Order/month-revenue");
       } else if (isManager) {
-        data = await get(`http://localhost:5264/api/Order/month-revenue/${account.storeId}`);
+        data = await get(`http://172.20.10.5:5264/api/Order/month-revenue/${account.storeId}`);
       }
       console.log("data-revenue", data);
       setMonthRevenue(data);
