f919d72dd8885a41fac96a91b7b677e7591d3976
Le Duc Dat <datldhe171371@fpt.edu.vn>
2024-07-01 23:31:55 +0700

    fix bootrap nodejs

--- FE/src/pages/Client/Blog/BlogDetail.js
+++ FE/src/pages/Client/Blog/BlogDetail.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link, BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { get } from "../../../helpers/API.helper";
-import { Button, Col, Row } from "antd";
+import { Button, Col, Row, Container } from "react-bootstrap";
 import "./Blog.css";
+import { Badge } from "antd";
 
 function BlogDetail() {
   const { id } = useParams();
@@ -12,6 +13,7 @@ function BlogDetail() {
     const fetchBlogDetail = async () => {
       const data = await get(`http://localhost:5264/api/Post/${id}`);
       setBlog(data);
+      console.log(data);
     };
     fetchBlogDetail();
   }, [id]);
@@ -29,12 +31,12 @@ function BlogDetail() {
     <>
       <div className="main-content">
         <div className="breadcrumb-area breadcrumb-height" data-bg-image="assets/images/breadcrumb/bg/1-1-1919x388.jpg">
-          <div className="container h-100">
-            <div className="row h-100">
-              <div className="col-lg-12">
-                <div className="breadcrumb-item">
+          <Container className="h-100">
+            <Row className="h-100 align-items-center">
+              <Col>
+                <div className="breadcrumb-item text-center">
                   <h2 className="breadcrumb-heading">Blog Detail</h2>
-                  <ul>
+                  <ul className="breadcrumb-list">
                     <li>
                       <a href="/">Home</a>
                     </li>
@@ -44,38 +46,40 @@ function BlogDetail() {
                     <li>{blog.title}</li>
                   </ul>
                 </div>
-              </div>
-            </div>
-          </div>
+              </Col>
+            </Row>
+          </Container>
         </div>
 
         <div className="blog-detail-area section-space-y-axis-100">
-          <div className="container">
-            <div className="row">
-              <Col span={24}>
+          <Container>
+            <Row>
+              <Col md={{ span: 8, offset: 2 }}>
                 <div className="blog-detail-content">
                   <h1 className="blog-detail-title">{blog.title}</h1>
-                  <div className="blog-detail-meta">
+                  <div className="blog-detail-meta d-flex justify-content-between">
                     <span className="author">By: {blog.author}</span>
                     <span className="date">{formatDate(blog.createdDate)}</span>
                   </div>
                   <div className="blog-detail-img">
-                    <img src={blog.img} alt="Blog" />
+                    <img src={blog.img} alt="Blog" className="img-fluid" />
                   </div>
-                  <div className="blog-detail-text">{blog.contents}</div>
+                  <div className="blog-detail-text mt-4">{blog.contents}</div>
+                  <div className="blog-detail-tags mt-4">
+                    <h5>Tags:</h5>
+                    <div color="blue">
+                    {blog.tags}
+                    </div>
+                  </div>
+                  <div className="back-to-blog text-center mt-5">
+                    <Button variant="primary">
+                      <Link to="/blog" className="text-white">Back to Blog</Link>
+                    </Button>
+                  </div>
                 </div>
               </Col>
-            </div>
-            <div className="row">
-              <Col span={24}>
-                <div className="back-to-blog">
-                  <Button type="primary">
-                    <Link to="/blog">Back to Blog</Link>
-                  </Button>
-                </div>
-              </Col>
-            </div>
-          </div>
+            </Row>
+          </Container>
         </div>
       </div>
 
@@ -98,101 +102,27 @@ function BlogDetail() {
             font-weight: bold;
           }
 
-          .breadcrumb-item ul {
+          .breadcrumb-list {
             list-style: none;
             padding: 0;
+            display: flex;
+            justify-content: center;
+            color: #000;
           }
 
-          .breadcrumb-item ul li {
+          .breadcrumb-list li {
             display: inline;
-            color: #fff;
+            color: #000;
             margin: 0 10px;
           }
 
-          .breadcrumb-item ul li a {
-            color: #fff;
+          .breadcrumb-list li a {
+            color: #000;
             text-decoration: none;
-          }
-
-          .blog-area {
-            padding: 50px 0;
-          }
-
-          .blog-item-wrap {
-            display: flex;
-            flex-wrap: wrap;
-            gap: 30px;
-          }
-
-          .blog-item {
-            border: 1px solid #ddd;
-            border-radius: 5px;
-            overflow: hidden;
-            background: #fff;
-          }
-
-          .blog-content {
-            padding: 20px;
-          }
-
-          .blog-meta {
-            margin-bottom: 10px;
-          }
-
-          .blog-meta ul {
-            list-style: none;
-            padding: 0;
-          }
-
-          .blog-meta ul li {
-            display: inline;
-            color: #888;
-            margin-right: 10px;
-          }
-
-          .blog-img {
-            position: relative;
-          }
-
-          .blog-img img {
-            width: 100%;
-            display: block;
-          }
-
-          .inner-btn-wrap {
-            position: absolute;
-            top: 10px;
-            right: 10px;
-          }
-
-          .inner-btn {
-            background: rgba(0, 0, 0, 0.6);
-            padding: 10px;
-            border-radius: 50%;
-            color: #fff;
-          }
-
-          .pagination-area {
-            margin-top: 20px;
-            text-align: right;
-          }
-
-          .pagination .page-link {
-            color: #007bff;
-          }
-
-          .pagination .page-item.active .page-link {
-            background-color: #007bff;
-            border-color: #007bff;
-            color: #fff;
           }
 
           .blog-detail-area {
             padding: 50px 0;
-          }
-
-          .blog-detail-content {
-            text-align: center;
           }
 
           .blog-detail-title {
@@ -204,16 +134,6 @@ function BlogDetail() {
             font-size: 16px;
             margin-bottom: 20px;
             color: #888;
-            display: flex;
-            justify-content: space-between;
-          }
-
-          .blog-detail-meta .author {
-            text-align: left;
-          }
-
-          .blog-detail-meta .date {
-            text-align: right;
           }
 
           .blog-detail-img img {
@@ -232,7 +152,11 @@ function BlogDetail() {
 
           .back-to-blog {
             margin-top: 30px;
-            text-align: center;
+          }
+
+          .back-to-blog .btn {
+            background-color: #007bff;
+            border-color: #007bff;
           }
         `}
       </style>
